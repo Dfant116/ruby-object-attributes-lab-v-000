423fcee531f80ac58d6c7007a537b355cdc5e2f9
Dalton Fant <daltonfant16@gmail.com>
2018-01-11 02:55:48 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,6 +11,22 @@ class Dog
   def name= (new_name)
     @name = new_name
   end
+
+  def dog (breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed= (new_breed)
+    @breed = new_breed
+  end
 end
 
+
 fido = Dog.new
+snoopy.breed = "Beagle"
+
+
